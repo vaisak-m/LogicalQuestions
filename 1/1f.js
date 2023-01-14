f282ab37ba5c9a4c98c299d8c345f6fd177c0d49
vaisak-m <90459333+vaisak-m@users.noreply.github.com>
2023-01-14 15:11:36 +0530

    Add files via upload

--- 1/1f.js
+++ 1/1f.js
@@ -1,24 +1,30 @@
-let passenger = ["Anand", "Vai", "Jewel", "Vaisak", "Abhijith", "Fazna", "Malavika"];
+let passenger = ["Anand", "Vaisak", "Jewel", "Vaisak", "Abhijith", "Fazna", "Malavika"];
 let index = 0;
+let store = [];
 
-for(let i = 0; i<passenger.length ;i++)
-{
-for(let j = i+1; j<=passenger.length ;j++)
-{
-    if(passenger[i]==passenger[j])
-    {
-        index = 1;
-    }
-}
+// for(let i = 0; i<passenger.length ;i++)
+// {
+// for(let j = i+1; j<=passenger.length ;j++)
+// {
+//     if(passenger[i]==passenger[j])
+//     {
+//         index = 1;
+//     }
+// }
 
+// if(index ==1)
+// {
+//     console.log(passenger[i]);
+//     store[i]=passenger[i];
+// }
+// else
+// {
+//     console.log("no duplicate")
+// }
 
-}
+// }
+// console.log(store);
 
-if(index ==1)
-{
-    console.log("duplicate found")
-}
-else
-{
-    console.log("no duplicate")
-}
+const dps = passenger.filter((i, index) => passenger.indexOf(i) !== index)
+
+console.log('Duplicate',dps);
